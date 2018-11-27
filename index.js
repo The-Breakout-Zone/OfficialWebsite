b148b5a11570218e09cd54297f0822d072fe1036
sharandasheep <31903966+sharandasheep@users.noreply.github.com>
2018-11-27 16:05:02 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){ 
+/*Turn on and off audio*/
+   var clicked = true;
+    $(".line-c").click( function() {
+  		 if (clicked) {
+    	 $(".line").addClass("stop");
+   		 clicked = false;
+         console.log("clicked true");
+          $("#backgroundaudio").trigger("pause");
+    } else {
+   		 $(".line").removeClass("stop");
+   		 clicked = true;
+         console.log("clicked false");
+         $("#backgroundaudio").trigger("play");
+  		}
+	});
+    
+ });
